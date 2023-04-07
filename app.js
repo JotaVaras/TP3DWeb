534287453c4214c04f279b8f0091e4b49cc6b46d
JotaVaras <varas4575@gmail.com>
2023-04-07 17:49:22 -0300

    termine el segundo section y agregue img

---
+++ app.js
@@ -0,0 +1,7 @@
+// window.addEventListener("scroll",function(){
+//     var header =TP3deDWeb.querySelector("header");
+//     header.classList.toggle('down',window.scrollY > 0);
+
+// })
+
+// revisar el como poner el menu fijo
