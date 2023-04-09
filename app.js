be83ab66bc708c2903f4639c4ac18fac40d9cbd8
JotaVaras <varas4575@gmail.com>
2023-04-09 18:38:05 -0300

    todo terminado

--- app.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener("scroll",function(){
-//     var header =TP3deDWeb.querySelector("header");
-//     header.classList.toggle('down',window.scrollY > 0);
-
-// })
-
-// revisar el como poner el menu fijo
